06d06446f20e76494b36e8d02521c59e2c513bfc
V Kiran Kumar Reddy <vkkr0424.dom@gmail.com>
2025-10-06 19:28:33 +0530

    updated data_download to use kagglehub

--- src/data_download.py
+++ src/data_download.py
@@ -1,16 +1,29 @@
 import os
-from kaggle.api.kaggle_api_extended import KaggleApi
+import zipfile
+import kagglehub
+import shutil
 
+# Folder where raw data will be stored
 DATA_DIR = "data/raw"
 os.makedirs(DATA_DIR, exist_ok=True)
 
-# Initialize Kaggle API
-api = KaggleApi()
-api.authenticate()
+# Step 1: Download dataset (downloads to current directory)
+zip_path = kagglehub.dataset_download("mlg-ulb/creditcardfraud")
+print("✅ Dataset downloaded at:", zip_path)
 
-# Kaggle dataset path
-dataset_name = "mldata/creditcardfraud"  # Update if needed
+filename = "creditcard.csv"
 
-# Download and unzip
-api.dataset_download_files(dataset_name, path=DATA_DIR, unzip=True)
-print(f"✅ Dataset downloaded to {DATA_DIR}")
+# Step 2: Move zip to DATA_DIR
+dst_path = os.path.join(DATA_DIR, filename, )
+src_path = os.path.join(zip_path, filename)
+shutil.copy(src_path, dst_path)
+
+print(f"Data moved to {DATA_DIR}")
+
+# # Step 3: Unzip
+# with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+#     zip_ref.extractall(DATA_DIR)
+# print(f"✅ Dataset extracted to {DATA_DIR}")
+
+# # Step 4: Remove zip to save space
+# os.remove(zip_path)
